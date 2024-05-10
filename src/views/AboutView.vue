1ec47efb1a247017e502f4f927b9957f689757ad
cieme <aslslgood22@163.com>
2024-05-10 15:32:29 +0800

    feat: 编辑器功能

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,79 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div ref="editor" class="editor"></div>
 </template>
+
+<script>
+import * as monaco from "monaco-editor";
+export default {
+  name: "MonacoEditor",
+  data() {
+    return {
+      code: `var a = 1;\nvar b=2;`,
+      editor: null,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.editor.dispose();
+  },
+  methods: {
+    init() {
+      // 初始化编辑器
+      this.editor = monaco.editor.create(this.$refs.editor, {
+        value: this.code,
+        language: "javascript",
+        theme: "vs-dark",
+        tabSize: 2,
+        scrollBeyondLastLine: false,
+        automaticLayout: true, // 自动布局
+        readOnly: false,
+      });
+      console.log(this.editor);
+
+      // 监听内容变化
+      this.editor.onDidChangeModelContent(() => {
+        console.log(this.editor.getValue());
+      });
+
+      // 监听失去焦点事件
+      this.editor.onDidBlurEditorText((e) => {
+        console.log(e);
+      });
+    },
+    // 获取编辑框内容
+    getCodeContext() {
+      return this.editor.getValue();
+    },
+    // 自动格式化代码
+    format() {
+      this.editor.trigger("anything", "editor.action.formatDocument");
+      // 或者
+      // this.editor.getAction(['editor.action.formatDocument']).run()
+      //或者
+      //自定义格式化 后赋值
+    },
+    changeEditor() {
+      if (this.editor === null) {
+        this.init();
+      }
+
+      const oldModel = this.editor.getModel();
+      const newModel = monaco.editor.createModel(this.code, "sql");
+
+      if (oldModel) {
+        oldModel.dispose();
+      }
+      this.editor.setModel(newModel);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.editor {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin(),
+      // new MonacoWebpackPlugin({
+      //   languages: ["javascript"], //configure your languages here
+      // }), // Place it here
+    ],
+  },
 });
