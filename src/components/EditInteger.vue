51c1d18ed3d7335ec0bfaa86eca554003e2eac28
cieme <aslslgood22@163.com>
2024-05-10 13:20:21 +0800

    feat: 暂存

---
+++ src/components/EditInteger.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: "EditInteger",
+  props: {
+    max: {
+      type: Number,
+      default: 1,
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  model: {
+    prop: "value",
+    event: "inputChange",
+  },
+  data() {
+    return {
+      beforeValue: "",
+    };
+  },
+  methods: {
+    focus() {
+      this.beforeValue = this.value;
+      this.processedLock = false;
+    },
+    blur(event) {
+      if (this.processedLock) return;
+      this.valueHandle(event);
+      this.processedLock = false;
+    },
+    valueHandle(event) {
+      const value = event.target.value.replace(/\s/g, "");
+      /* 只允许正整数，如果不符合规则,输入无效 */
+      const reg = /^[1-9]\d*$/;
+      if (!reg.test(value)) {
+        this.$emit("inputChange", this.beforeValue);
+        return;
+      }
+      // 抛出时间
+      this.$emit("inputChange", value); //
+      this.$emit("onChange", value); // 最终结束事件
+      /* 打开锁 */
+      this.processedLock = true;
+    },
+    inputChange(e) {
+      this.$emit("inputChange", e);
+    },
+  },
+};
+</script>
+<template>
+  <el-input
+    ref="refElement"
+    v-bind="$attrs"
+    v-on="$listeners"
+    :value="value"
+    @focus="focus"
+    @blur="blur"
+    @keydown.native.enter="valueHandle"
+    @input="inputChange"
+  >
+    <template v-for="(index, name) in $slots" :slot="name">
+      <slot :name="name"></slot>
+    </template>
+  </el-input>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,29 @@
   <div class="home">
     {{ time }}
     <InputInterval v-model="time" />
+    {{ integer }}
+    <EditInteger v-model="integer" />
+    {{ real }}
+    <EditReal v-model="real" />
   </div>
 </template>
 
 <script>
 import InputInterval from "@/components/InputInterval.vue";
+import EditInteger from "@/components/EditInteger.vue";
+import EditReal from "@/components/EditReal.vue";
 export default {
   name: "HomeView",
   components: {
     InputInterval,
+    EditInteger,
+    EditReal,
   },
   data() {
     return {
       time: "",
+      integer: "",
+      real: "",
     };
   },
 };
